22a7b3cef20dcf3ed01a817c7cc1a5bbd91317fa
Mkhzoumi <zx.hammam@hotmail.com>
2021-04-12 17:54:11 +0300

    rearrange

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 'use strict';
+//these variables are used inside all objects.
+let container=document.getElementById('cont');
+let locationName=document.createElement('h1');
+let unorderedList=document.createElement('ul');
+let amPm='';
+let list='';
+let total=0;
+let clock=5;
+
 //seattle
 const seatle = {
   minCust:23,
@@ -16,44 +25,48 @@ const seatle = {
     }
   },
 
+  render: function(){
+
+    container.appendChild(locationName);
+
+    container.appendChild(unorderedList);
+    locationName.textContent='Seattle';
+
+    let amPm='';
+    let list='';
+    let total=0;
+    let clock=5;
+
+
+    for(let i =0 ; i<15 ; i++){
+      list=document.createElement('li');
+      unorderedList.appendChild(list);
+      if (clock < 12 ){
+        clock++;
+      }else{
+        clock=1;
+      }if(clock < 12 && i<6){
+        amPm='am';
+      }
+      if(i>=6){
+        amPm='pm';
+      }
+      if(i<14){
+        list.textContent=`${clock} ${amPm}: ${seatle.cookPurchEachHr[i]}`;
+        total=total+seatle.cookPurchEachHr[i];
+      }else{
+        list.textContent=`total: ${total}`;
+      }
+
+    }
+
+  }
+
 };
+
 seatle.avgCust();
+seatle.render();
 
-let container=document.getElementById('cont');
-let locationName=document.createElement('h1');
-container.appendChild(locationName);
-locationName.textContent='Seattle';
-
-
-let unorderedList=document.createElement('ul');
-container.appendChild(unorderedList);
-
-let amPm='';
-let list='';
-let total=0;
-let clock=5;
-
-
-for(let i =0 ; i<15 ; i++){
-  list=document.createElement('li');
-  unorderedList.appendChild(list);
-  if (clock < 12 ){
-    clock++;
-  }else{
-    clock=1;
-  }if(clock < 12 && i<6){
-    amPm='am';
-  }
-  if(i>=6){
-    amPm='pm';
-  }
-  if(i<14){
-    list.textContent=`${clock} ${amPm}: ${seatle.cookPurchEachHr[i]}`;
-    total=total+seatle.cookPurchEachHr[i];
-  }else{
-    list.textContent=`total: ${total}`;
-  }
-}
 
 
 //tokyo
@@ -73,44 +86,46 @@ const tokyo = {
     }
   },
 
+  render: function(){
+    container=document.getElementById('cont');
+    locationName=document.createElement('h1');
+    container.appendChild(locationName);
+    locationName.textContent='Tokyo';
+
+
+    unorderedList=document.createElement('ul');
+    container.appendChild(unorderedList);
+
+
+
+
+    for(let i =0 ; i<15 ; i++){
+      list=document.createElement('li');
+      unorderedList.appendChild(list);
+      if (clock < 12 ){
+        clock++;
+      }else{
+        clock=1;
+      }if(clock < 12 && i<6){
+        amPm='am';
+      }
+      if(i>=6){
+        amPm='pm';
+      }
+      if(i<14){
+        list.textContent=`${clock} ${amPm}: ${tokyo.cookPurchEachHr[i]}`;
+        total=total+tokyo.cookPurchEachHr[i];
+      }else{
+        list.textContent=`total: ${total}`;
+      }
+    }
+
+  }
 };
+
 tokyo.avgCust();
+tokyo.render();
 
-container=document.getElementById('cont');
-locationName=document.createElement('h1');
-container.appendChild(locationName);
-locationName.textContent='Tokyo';
-
-
-unorderedList=document.createElement('ul');
-container.appendChild(unorderedList);
-
-amPm='';
-list='';
-total=0;
-clock=5;
-
-
-for(let i =0 ; i<15 ; i++){
-  list=document.createElement('li');
-  unorderedList.appendChild(list);
-  if (clock < 12 ){
-    clock++;
-  }else{
-    clock=1;
-  }if(clock < 12 && i<6){
-    amPm='am';
-  }
-  if(i>=6){
-    amPm='pm';
-  }
-  if(i<14){
-    list.textContent=`${clock} ${amPm}: ${tokyo.cookPurchEachHr[i]}`;
-    total=total+tokyo.cookPurchEachHr[i];
-  }else{
-    list.textContent=`total: ${total}`;
-  }
-}
 
 
 //Dubai
@@ -130,43 +145,49 @@ const dubai = {
     }
   },
 
+  render: function(){
+    container=document.getElementById('cont');
+    locationName=document.createElement('h1');
+    container.appendChild(locationName);
+    locationName.textContent='Dubai';
+
+
+    unorderedList=document.createElement('ul');
+    container.appendChild(unorderedList);
+
+    amPm='';
+    list='';
+    total=0;
+    clock=5;
+
+    for(let i =0 ; i<15 ; i++){
+      list=document.createElement('li');
+      unorderedList.appendChild(list);
+      if (clock < 12 ){
+        clock++;
+      }else{
+        clock=1;
+      }if(clock < 12 && i<6){
+        amPm='am';
+      }
+      if(i>=6){
+        amPm='pm';
+      }
+      if(i<14){
+        list.textContent=`${clock} ${amPm}: ${dubai.cookPurchEachHr[i]}`;
+        total=total+dubai.cookPurchEachHr[i];
+      }else{
+        list.textContent=`total: ${total}`;
+      }
+    }
+
+  }
+
 };
+
 dubai.avgCust();
+dubai.render();
 
-container=document.getElementById('cont');
-locationName=document.createElement('h1');
-container.appendChild(locationName);
-locationName.textContent='Dubai';
-
-
-unorderedList=document.createElement('ul');
-container.appendChild(unorderedList);
-
-amPm='';
-list='';
-total=0;
-clock=5;
-
-for(let i =0 ; i<15 ; i++){
-  list=document.createElement('li');
-  unorderedList.appendChild(list);
-  if (clock < 12 ){
-    clock++;
-  }else{
-    clock=1;
-  }if(clock < 12 && i<6){
-    amPm='am';
-  }
-  if(i>=6){
-    amPm='pm';
-  }
-  if(i<14){
-    list.textContent=`${clock} ${amPm}: ${dubai.cookPurchEachHr[i]}`;
-    total=total+dubai.cookPurchEachHr[i];
-  }else{
-    list.textContent=`total: ${total}`;
-  }
-}
 
 
 //paris
@@ -185,44 +206,49 @@ const paris = {
       this.cookPurchEachHr[i]=Math.floor(this.avgCustPerHr[i]*this.avgCookie);
     }
   },
+  render: function(){
+    container=document.getElementById('cont');
+    locationName=document.createElement('h1');
+    container.appendChild(locationName);
+    locationName.textContent='Paris';
+
+
+    unorderedList=document.createElement('ul');
+    container.appendChild(unorderedList);
+
+    amPm='';
+    list='';
+    total=0;
+    clock=5;
+
+    for(let i =0 ; i<15 ; i++){
+      list=document.createElement('li');
+      unorderedList.appendChild(list);
+      if (clock < 12 ){
+        clock++;
+      }else{
+        clock=1;
+      }if(clock < 12 && i<6){
+        amPm='am';
+      }
+      if(i>=6){
+        amPm='pm';
+      }
+      if(i<14){
+        list.textContent=`${clock} ${amPm}: ${paris.cookPurchEachHr[i]}`;
+        total=total+paris.cookPurchEachHr[i];
+      }else{
+        list.textContent=`total: ${total}`;
+      }
+    }
+
+  }
 
 };
+
 paris.avgCust();
+paris.render();
 
-container=document.getElementById('cont');
-locationName=document.createElement('h1');
-container.appendChild(locationName);
-locationName.textContent='Paris';
-
-
-unorderedList=document.createElement('ul');
-container.appendChild(unorderedList);
-
-amPm='';
-list='';
-total=0;
-clock=5;
-
-for(let i =0 ; i<15 ; i++){
-  list=document.createElement('li');
-  unorderedList.appendChild(list);
-  if (clock < 12 ){
-    clock++;
-  }else{
-    clock=1;
-  }if(clock < 12 && i<6){
-    amPm='am';
-  }
-  if(i>=6){
-    amPm='pm';
-  }
-  if(i<14){
-    list.textContent=`${clock} ${amPm}: ${paris.cookPurchEachHr[i]}`;
-    total=total+paris.cookPurchEachHr[i];
-  }else{
-    list.textContent=`total: ${total}`;
-  }
-}
 
 //lima
 const lima = {
@@ -240,44 +266,48 @@ const lima = {
       this.cookPurchEachHr[i]=Math.floor(this.avgCustPerHr[i]*this.avgCookie);
     }
   },
+  render: function(){
+    container=document.getElementById('cont');
+    locationName=document.createElement('h1');
+    container.appendChild(locationName);
+    locationName.textContent='Lima';
 
+
+    unorderedList=document.createElement('ul');
+    container.appendChild(unorderedList);
+
+    amPm='';
+    list='';
+    total=0;
+    clock=5;
+
+    for(let i =0 ; i<15 ; i++){
+      list=document.createElement('li');
+      unorderedList.appendChild(list);
+      if (clock < 12 ){
+        clock++;
+      }else{
+        clock=1;
+      }if(clock < 12 && i<6){
+        amPm='am';
+      }
+      if(i>=6){
+        amPm='pm';
+      }
+      if(i<14){
+        list.textContent=`${clock} ${amPm}: ${lima.cookPurchEachHr[i]}`;
+        total=total+lima.cookPurchEachHr[i];
+      }else{
+        list.textContent=`total: ${total}`;
+      }
+    }
+
+  }
 };
+
 lima.avgCust();
+lima.render();
 
-container=document.getElementById('cont');
-locationName=document.createElement('h1');
-container.appendChild(locationName);
-locationName.textContent='Lima';
-
-
-unorderedList=document.createElement('ul');
-container.appendChild(unorderedList);
-
-amPm='';
-list='';
-total=0;
-clock=5;
-
-for(let i =0 ; i<15 ; i++){
-  list=document.createElement('li');
-  unorderedList.appendChild(list);
-  if (clock < 12 ){
-    clock++;
-  }else{
-    clock=1;
-  }if(clock < 12 && i<6){
-    amPm='am';
-  }
-  if(i>=6){
-    amPm='pm';
-  }
-  if(i<14){
-    list.textContent=`${clock} ${amPm}: ${lima.cookPurchEachHr[i]}`;
-    total=total+lima.cookPurchEachHr[i];
-  }else{
-    list.textContent=`total: ${total}`;
-  }
-}
 
 
 
